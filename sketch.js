bf44504262cf628c62637b53270865da3f1b751c
B0tta <90757307+B0tta@users.noreply.github.com>
2021-11-10 08:51:39 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,69 @@
+const phrase1 = "Slim Shady";
+const phrase2 = "Marshall Mathers";
+const words1 = phrase1.split(" ");
+const words2 = phrase2.split(" ");
+let buttons1;
+let buttons2;
+let iterator = 0;
+let eminem;
+let slim;
+
 function preload() {
-  // put preload code here
+  eminem = loadImage("./assets/eminem.png");
+  slim = loadImage("./assets/slimshady.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  imageMode(CENTER);
+
+  words1.forEach((word) => {
+    createElement("button", word);
+  });
+  buttons1 = selectAll("button");
+
+  words2.forEach((word) => {
+    createElement("button", word);
+  });
+  buttons2 = selectAll("button");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  buttons2.forEach((button, i) => {
+    let x = noise((iterator + 400 * i) / 400) * windowWidth;
+    let y = noise((iterator - 400 * i) / 400) * windowHeight;
+    button.position(40000, 40000);
+  });
+  if (mouseIsPressed == false) {
+    background(171, 49, 24);
+    slimimg = image(slim, width / 2, height / 2);
+    iterator++;
+    buttons1.forEach((button, i) => {
+      let x = noise((iterator + 400 * i) / 400) * windowWidth;
+      let y = noise((iterator - 400 * i) / 400) * windowHeight;
+      button.position(x, y);
+    });
+  } else if (mouseIsPressed == true) {
+    background(167, 198, 235);
+    eminemimg = image(eminem, width / 2, height / 2);
+    iterator++;
+    buttons2.forEach((button, i) => {
+      let x = noise((iterator + 400 * i) / 400) * windowWidth;
+      let y = noise((iterator - 400 * i) / 400) * windowHeight;
+      button.position(x, y);
+    });
+    buttons1.forEach((button, i) => {
+      let x = noise((iterator + 400 * i) / 400) * windowWidth;
+      let y = noise((iterator - 400 * i) / 400) * windowHeight;
+      button.position(40000, 40000);
+    });
+  }
+
+  var firstText = "Hi! My name is...";
+
+  textAlign(CENTER);
+  textSize(30);
+  fill(0, 0, 0);
+
+  text(firstText, width / 2, 100);
 }
